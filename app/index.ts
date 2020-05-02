a3d259e292bc46de3fab78ee0c7f029e41f60fa3
VeryDifficult <vsellenay.pp@gmail.com>
2020-05-02 15:58:41 +0300

    ADD: express

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,10 @@
-console.log('asd');
+import express from 'express';
+
+const app: express.Application = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(8081, () => {
+  console.log('8081');
+});
