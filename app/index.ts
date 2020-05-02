497b69f0601c295e90b6527e1587e39b044f57b9
krivenkop <ukr.empresario@gmail.com>
2020-05-02 19:19:20 +0300

    ADD: test db connection in server

--- app/index.ts
+++ app/index.ts
@@ -1,16 +1,19 @@
 import * as express from 'express';
-import { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
+import { Connection } from '../lib/connection';
+
+import { Request, Response } from 'express';
+import {QueryResult} from "pg";
 
 dotenv.config();
 
 const app: express.Application = express();
 const {PORT} = process.env;
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({
-    message: 'Hello world',
-  });
+app.get('/', async (req: Request, res: Response) => {
+  const dbRes: QueryResult = await (new Connection).fetchDatabases();
+  console.log(dbRes.rows);
+  res.json(dbRes);
 });
 
 app.listen(PORT, () => {
