bf96ad1369e2f38945dab768fbcccbd5ebf1bc1c
krivenkop <ukr.empresario@gmail.com>
2020-05-09 14:34:33 +0300

    ADD: TypeORM

--- app/app.ts
+++ app/app.ts
@@ -5,7 +5,13 @@ import { Connection } from '@lib/connection';
 import { Request, Response } from 'express';
 import { QueryResult } from 'pg';
 
+import 'reflect-metadata';
+import { connect } from '@app/orm';
+
 dotenv.config();
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const connection = connect();
 
 export const app: express.Application = express();
 
